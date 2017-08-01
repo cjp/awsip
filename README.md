5d7db5b1e54ed239f2e38f4d9ff6cca0ac329eaf
Christopher J. Pilkington <cjp@0x1.net>
2017-08-01 17:35:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# awsip
+
+Looks up current PrivateIpAddress of a named EC2 instance
+
+usage: awsip <instance_name>
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Looks up current PrivateIpAddress of a named EC2 instance\n\n")
+	fmt.Fprintf(os.Stderr, "usage: %s <instance_name>\n", filepath.Base(os.Args[0]))
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		usage()
+		os.Exit(64)
+	}
+
+	inst := os.Args[1]
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	ec2client := ec2.New(sess)
+
+	params := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			{
+				Name:   aws.String("tag:Name"),
+				Values: []*string{aws.String(inst)},
+			},
+			{
+				Name:   aws.String("instance-state-name"),
+				Values: []*string{aws.String("running")},
+			},
+		},
+	}
+
+	resp, err := ec2client.DescribeInstances(params)
+	if err != nil {
+		log.Fatal("Error listing instances", err)
+	}
+
+	var ip string = ""
+	for idx := range resp.Reservations {
+		for _, inst := range resp.Reservations[idx].Instances {
+			ip = *inst.PrivateIpAddress
+		}
+	}
+
+	if len(ip) > 0 {
+		fmt.Println(ip)
+	} else {
+		fmt.Fprintf(os.Stderr, "Not found\n")
+		os.Exit(1)
+	}
+}
